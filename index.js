3ccf1a460cf788aa9b4bbc1662420d9270923e0e
giri kalra <giiirrriii@gmail.com>
2017-08-31 18:56:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 101);
+ cart.push({[item] : price});
+ console.log(item + " has been added to your cart.");
+ return cart;
+ 
 }
 
 function viewCart() {

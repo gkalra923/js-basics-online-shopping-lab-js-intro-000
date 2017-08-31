9571b96759375edc984a43a0df9dbd809209a9ca
giri kalra <giiirrriii@gmail.com>
2017-08-31 19:34:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,4 +78,11 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a card on file for you.")
+  }
+  else {
+    console.log("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".");
+    cart = [];
+  }
 }
